016c818797eafb913096c19e536c4e59faad9f1e
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-25 16:49:10 +0200

    Add updatedTasklist

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,22 +1,36 @@
 import "./AddTask.css";
 
-export const AddTask = ( {tasklist, setTasklist} ) => {
+export const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const date = new Date();
-    // console.log(e.target.task.value);
-    // console.log(date.getTime);
-    const newTask = {
-      id: date.getTime(),
-      name: e.target.task.value,
-      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
-    }
+    if (task.id) {
+      const date = new Date();
+const updatedTasklist = tasklist.map((todo) =>
+  todo.id === task.id
+    ? {
+        id: task.id,
+        name: task.name,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      }
+    : todo
+);
+setTasklist(updatedTasklist);
+    } else {
+      const date = new Date();
+      // console.log(e.target.task.value);
+      // console.log(date.getTime);
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
 
-    setTasklist([...tasklist, newTask]);
-    // Clear submit field after adding task:
-    e.target.task.value = "";
+      setTasklist([...tasklist, newTask]);
+      // Clear submit field after adding task:
+      e.target.task.value = "";
+    }
   }
 
   return (
@@ -25,9 +39,12 @@ export const AddTask = ( {tasklist, setTasklist} ) => {
         <input
           type="text"
           name="task"
+          value={task.name}
           autoComplete="off"
           placeholder="New task title"
           maxLength="25"
+          // Added in order to change task title:
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
         />
         <button type="submit">Add</button>
       </form>
