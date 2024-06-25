2ada43f9d5f12069d0cdd4c7b59e0f653004e146
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-25 15:52:17 +0200

    Add handleSubmit function

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,27 @@
 import "./AddTask.css";
 
 export const AddTask = ( {tasklist, setTasklist} ) => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const date = new Date();
+    // console.log(e.target.task.value);
+    // console.log(date.getTime);
+    const newTask = {
+      id: date.getTime(),
+      name: e.target.task.value,
+      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+    }
+
+    setTasklist([...tasklist, newTask]);
+    // Clear submit field after adding task:
+    e.target.task.value = "";
+  }
+
   return (
     <section className="addTask">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="task"
