f5435ca45a23ad58615a10d8424baf8778cf0149
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-25 14:59:01 +0200

    Import ShwTask component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
+import { ShowTask } from "./components/ShowTask";
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <AddTask />
+      <ShowTask />
     </div>
   );
 }
