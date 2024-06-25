09278f34d453c7f17033bbaa6ff39e99c9be4765
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-25 14:45:09 +0200

    Import AddTask component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
+import { AddTask } from "./components/AddTask";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Header />
+      <Header />
+      <AddTask />
     </div>
   );
 }
